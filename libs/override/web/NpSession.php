d5c6ca03be83d6f744c810370fe1559475e1c355
Trac Nguyen <trac.nguyen@news.com.au>
2017-09-11 15:05:15 +0700

    Add migration and Seesion

---
+++ libs/override/web/NpSession.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: npbtrac@yahoo.com
+ * Date time: 9/11/17 1:46 PM
+ */
+
+namespace enpii\enpiiCms\libs\override\web;
+
+
+use yii\web\Session;
+
+/**
+ * Class NpSession
+ * @package enpii\enpiiCms\libs\override\web
+ */
+class NpSession extends Session
+{
+
+}
