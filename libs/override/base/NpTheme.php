819f721f13420405e39f9de626cb57ddaa9a9a8a
Trac Nguyen <trac.nguyen@news.com.au>
2017-09-29 00:27:45 +0700

    Add theme class

---
+++ libs/override/base/NpTheme.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: npbtrac@yahoo.com
+ * Date time: 9/29/17 12:25 AM
+ */
+
+namespace enpii\enpiiCms\libs\override\base;
+
+
+use yii\base\Theme;
+
+/**
+ * Class NpTheme
+ * @package enpii\enpiiCms\libs\override\base
+ */
+class NpTheme extends Theme
+{
+
+}
