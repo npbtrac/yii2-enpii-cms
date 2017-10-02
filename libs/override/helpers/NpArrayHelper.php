6e3f40051a13e3a76e05a7294e254e825d70ef27
Trac Nguyen <trac.nguyen@news.com.au>
2017-10-02 15:01:08 +0700

    Add ArrayHelper

---
+++ libs/override/helpers/NpArrayHelper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: npbtrac@yahoo.com
+ * Date time: 10/2/17 11:49 AM
+ */
+
+namespace enpii\enpiiCms\libs\override\helpers;
+
+/**
+ * Class NpArrayHelper
+ * @package enpii\enpiiCms
+ */
+class NpArrayHelper extends \yii\helpers\ArrayHelper
+{
+
+}
