de9e019ed1e182ab1703347c0c8455c6afafde4b
Trac Nguyen <trac.nguyen@news.com.au>
2017-10-03 23:01:57 +0700

    Add menu widget

--- libs/override/db/NpMigration.php
+++ libs/override/db/NpMigration.php
@@ -33,35 +33,37 @@ class NpMigration extends Migration
      * Use if identity an item alternatively to ID
      * @param $tableName
      */
-    public function addCode($tableName)
+    public function addCode($tableName, $columnName = 'code')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'code',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->string(32)->comment('Code for this item, used identifying when needed. Another criteria to replace ID because ID is auto-increment'));
-        $this->createIndex($tableName . '_code' . '_idx', '{{%' . $tableName . '}}', 'code');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}', $columnName);
     }
 
     /**
      * Add and put Index to created_at column
      * Store the datetime the record has been created (should be stored in GMT 0)
      * @param $tableName
+     * @param string $columnName Name of the column
      */
-    public function addCreatedAt($tableName)
+    public function addCreatedAt($tableName, $columnName = 'created_at_gmt')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'created_at_gmt',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->dateTime()->comment('Date and time this record created (in GMT)'));
-        $this->createIndex($tableName . '_' . 'created_at_gmt' . '_idx', '{{%' . $tableName . '}}', 'created_at_gmt');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}', $columnName);
     }
 
     /**
      * Add and put Index to updated_at column
      * Store the datetime the record has been updated (should be stored in GMT 0)
      * @param $tableName
+     * @param string $columnName Name of the column
      */
-    public function addUpdatedAt($tableName)
+    public function addUpdatedAt($tableName, $columnName = 'updated_at_gmt')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'updated_at_gmt',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->dateTime()->comment('Date and time this record updated (in GMT)'));
-        $this->createIndex($tableName . '_' . 'updated_at_gmt' . '_idx', '{{%' . $tableName . '}}', 'updated_at_gmt');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}', $columnName);
     }
 
     /**
@@ -70,12 +72,12 @@ class NpMigration extends Migration
      * a scheduled time in the future)
      * @param $tableName
      */
-    public function addPublishedAt($tableName)
+    public function addPublishedAt($tableName, $columnName = 'published_at_gmt')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'published_at_gmt',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->dateTime()->comment('Date and time this record published (in GMT)'));
-        $this->createIndex($tableName . '_' . 'published_at_gmt' . '_idx', '{{%' . $tableName . '}}',
-            'published_at_gmt');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}',
+            $columnName);
     }
 
     /**
@@ -83,11 +85,11 @@ class NpMigration extends Migration
      * Store id of user who create this record
      * @param $tableName
      */
-    public function addCreatorID($tableName)
+    public function addCreatorID($tableName, $columnName = 'creator_id')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'creator_id',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->bigInteger()->comment('ID of user who created this item'));
-        $this->createIndex($tableName . '_' . 'creator_id' . '_idx', '{{%' . $tableName . '}}', 'creator_id');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}', $columnName);
     }
 
     /**
@@ -95,11 +97,11 @@ class NpMigration extends Migration
      * This flag (0 or 1) for putting it to trash without completely delete it off the database
      * @param $tableName
      */
-    public function addIsDeleted($tableName)
+    public function addIsDeleted($tableName, $columnName = 'is_deleted')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'is_deleted',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->boolean()->defaultValue(0)->comment('Mark an item is deleted (in trash) or not'));
-        $this->createIndex($tableName . '_is_deleted' . '_idx', '{{%' . $tableName . '}}', 'is_deleted');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}', $columnName);
     }
 
     /**
@@ -107,11 +109,11 @@ class NpMigration extends Migration
      * This flag (0 or 1) for allowing it to be displayed in the frontend or not
      * @param $tableName
      */
-    public function addIsEnabled($tableName)
+    public function addIsEnabled($tableName, $columnName = 'is_enabled')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'is_enabled',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->boolean()->defaultValue(1)->comment('Mark an item is allowed on frontend query or not'));
-        $this->createIndex($tableName . '_is_enabled' . '_idx', '{{%' . $tableName . '}}', 'is_enabled');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}', $columnName);
     }
 
     /**
@@ -119,11 +121,11 @@ class NpMigration extends Migration
      * For custom order in sorting to be displayed in the frontend
      * @param $tableName
      */
-    public function addStatus($tableName)
+    public function addStatus($tableName, $columnName = 'status')
     {
-        $this->addColumn('{{%' . $tableName . '}}', 'status',
+        $this->addColumn('{{%' . $tableName . '}}', $columnName,
             $this->smallInteger()->notNull()->defaultValue(1)->comment('Status of this item, 0: draft, not publised, 1: published '));
-        $this->createIndex($tableName . '_status' . '_idx', '{{%' . $tableName . '}}', 'status');
+        $this->createIndex($tableName . '_' . $columnName . '_idx', '{{%' . $tableName . '}}', $columnName);
     }
 
     /**
---
+++ libs/override/widgets/NpMenu.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: npbtrac@yahoo.com
+ * Date time: 10/3/17 3:52 PM
+ */
+
+namespace enpii\enpiiCms\libs\override\widgets;
+
+use yii\widgets\Menu;
+
+/**
+ * Class NpMenu
+ * @package enpii\enpiiCms
+ */
+class NpMenu extends Menu
+{
+
+}
